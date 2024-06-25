5f984b4a6d00764e966badf60aab1c73a871086f
Benjamin Girard <b.girardpro@gmail.com>
2024-06-25 21:13:23 +0200

    create prisma module

---
+++ src/providers/prisma/prisma.module.ts
@@ -0,0 +1,5 @@
+import { Module, OnModuleInit } from '@nestjs/common'
+import { PrismaService } from './prisma.service'
+
+@Module({ providers: [PrismaService], exports: [PrismaService] })
+export class PrismaModule {}
