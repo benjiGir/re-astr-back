01505a6b972cc1e2db74240399964ee988b0f3e1
Benjamin Girard <b.girardpro@gmail.com>
2025-10-03 22:25:22 +0200

    🔥 remove: replace Prisma with Drizzle ORM

--- src/modules/Users/dto/params-user.dto.ts
+++
@@ -1,9 +0,0 @@
-import { ApiProperty } from '@nestjs/swagger'
-import { IsEnum } from 'class-validator'
-import { UserRole } from '../constants/user.contstant'
-
-export class ParamsGetUsersDto {
-  @ApiProperty({ enum: UserRole })
-  @IsEnum(UserRole)
-  userRole: UserRole
-}
